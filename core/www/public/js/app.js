bdc8f489bb7d37a1c7e7e08b9f3b1e4df5d32606
riza <rizasabuncu@outlook.com>
2017-11-28 22:09:05 +0300

    ios11 fix

--- core/www/public/js/app.js
+++ core/www/public/js/app.js
@@ -5,6 +5,56 @@ window.mobilecheck = function() {
   return check;
 };
 
+function copyToClipboard(elem) {
+  var targetId = "_hiddenCopyText_";
+  var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+  var origSelectionStart, origSelectionEnd;
+  if (isInput) {
+
+    target = elem;
+    origSelectionStart = elem.selectionStart;
+    origSelectionEnd = elem.selectionEnd;
+  } else {
+
+    target = document.getElementById(targetId);
+    if (!target) {
+      var target = document.createElement("textarea");
+      target.style.position = "absolute";
+      target.style.left = "-9999px";
+      target.style.top = "0";
+      target.id = targetId;
+      document.body.appendChild(target);
+    }
+    target.textContent = elem.textContent;
+  }
+
+  var currentFocus = document.activeElement;
+  target.focus();
+  target.setSelectionRange(0, target.value.length);
+
+
+  var succeed;
+  try {
+    succeed = document.execCommand("copy");
+  } catch(e) {
+    succeed = false;
+  }
+
+  if (currentFocus && typeof currentFocus.focus === "function") {
+    currentFocus.focus();
+  }
+
+  if (isInput) {
+
+    elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+  } else {
+
+    target.textContent = "";
+  }
+  return succeed;
+}
+
+
 $(() => {
 
   if (mobilecheck()) {
